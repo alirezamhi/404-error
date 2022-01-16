e96e2a29aa8b1ccd1b3d1ab5a9d526acb0114bf5
alireza mohammadkhani <alirezamohammadkhani2@gmail.com>
2022-01-16 11:24:24 +0330

    add same code

--- script/script.js
+++ script/script.js
@@ -1,10 +1,17 @@
 const container = document.querySelector('.container');
-container.addEventListener('mousemove' , myMouse);
-function myMouse(e){
-    let y = e.clientY / 5;
-    let x = e.clientX /5;
+
+// container.addEventListener('mousemove' , myMouse);
+// function myMouse(e){
+//     let y = e.clientY / 5;
+//     let x = e.clientX /5;
+
+//     container.style.backgroundPositionY = y + "px";
+//     container.style.backgroundPositionX = x + "px";
+// }
+window.onmousemove = function (event){
+    let y = event.clientY / 5;
+    let x = event.clientX /5;
 
     container.style.backgroundPositionY = y + "px";
     container.style.backgroundPositionX = x + "px";
 }
-
